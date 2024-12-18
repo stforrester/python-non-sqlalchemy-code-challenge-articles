5b925c88b19912780a7f05a0825297ae85cba8eb
Stuart Forrester <stforrester@gmail.com>
2024-12-18 13:27:08 -0600

    completes author and magazine initializers and properties

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -8,6 +8,15 @@ class Author:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0 and not hasattr(self, "name"):
+            self._name = value
+
     def articles(self):
         pass
 
@@ -24,6 +33,24 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) >= 2 and len(value) <= 16:
+            self._name = value
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
 
     def articles(self):
         pass
